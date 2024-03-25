58c98712f345aa56bbb5988b4e0de2eb0049b49f
Sergii Minko <minkosergii@gmail.com>
2024-03-25 21:01:57 +0100

    homework-5

--- py/test2.py
+++ py/test2.py
@@ -1,37 +1,18 @@
 from decimal import Decimal
+import re
 
 
+# Функція для генерування чисел з тексту
 def generator_numbers(text: str):
-    """
-    Генерує числа з тексту як генератор, використовуючи модуль decimal.
-
-    Args:
-        text (str): Рядок, який містить числа, розділені пробілами.
-
-    Yields:
-        Decimal: Дійсне число з тексту.
-    """
-    words = text.split()
-    print(f"words: {words}")
-    for word in words:
-        try:
-            number = Decimal(word)  # Використовуємо Decimal для точного обчислення
-            yield number
-        except ValueError:
-            pass
+    # Використовуємо регулярні вирази для знаходження чисел
+    pattern = r"[-+]?\d*\.\d+|\d+"
+    numbers = re.findall(pattern, text)
+    for number in numbers:
+        yield Decimal(number)  # Використовуємо Decimal для точного обчислення
 
 
+# Функція для обчислення загального прибутку
 def sum_profit(text: str, func):
-    """
-    Обчислює загальний прибуток з чисел у тексті, використовуючи модуль decimal.
-
-    Args:
-        text (str): Рядок, який містить числа, розділені пробілами.
-        func (Callable): Функція-генератор для отримання чисел.
-
-    Returns:
-        Decimal: Загальний прибуток.
-    """
     total_profit = Decimal(0)
     for number in func(text):
         total_profit += number
@@ -41,7 +22,6 @@ def sum_profit(text: str, func):
 # Приклад використання:
 def main():
     text = """Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."""
-    print(f"text: {text}")
     total_income = sum_profit(text, generator_numbers)
     print(f"Загальний дохід: {total_income}")
 
--- py/test4.py
+++ py/test4.py
@@ -1,3 +1,23 @@
+from functools import wraps
+
+
+# Декоратор для виправлення помилок.
+def input_error(func):
+    @wraps(func)
+    def inner(*args, **kwargs):
+        try:
+
+            return func(*args, **kwargs)
+        except ValueError:
+            return "Give me name and phone please."
+        except KeyError:
+            return "Contact not found. Use 'add' command instead"
+        except IndexError:
+            return "Enter the argument for the command"
+
+    return inner
+
+
 # Розбиває введене користувачем рядок на команду та аргументи.
 def parse_input(user_input):
     cmd, *args = user_input.split()
@@ -6,9 +26,8 @@ def parse_input(user_input):
 
 
 # Додає новий контакт до словника контактів.
+@input_error
 def add_contact(args, contacts):
-    if len(args) != 2:
-        return "Invalid command."
     if args[0] in contacts:
         return "Contact already exists. Use 'change' command instead."
     name, phone = args
@@ -17,9 +36,8 @@ def add_contact(args, contacts):
 
 
 # Змінює номер телефону існуючого контакту.
+@input_error
 def change_contact(args, contacts):
-    if len(args) != 2:
-        return "Invalid command."
     if args[0] not in contacts:
         return "Contact not found. Use 'add' command instead."
     name, phone = args
@@ -28,17 +46,16 @@ def change_contact(args, contacts):
 
 
 # Виводить телефонний номер для вказаного імені контакту.
+@input_error
 def show_phone(args, contacts):
-    if len(args) != 1:
-        return "Invalid command."
+    if args[0] not in contacts:
+        return "Contact not found. Use 'add' command instead."
     name = args[0]
-    if name in contacts:
-        return contacts[name]
-    else:
-        return "Contact not found."
+    return contacts[name]
 
 
 # Виводить усі контакти.
+@input_error
 def show_all(contacts):
     if len(contacts) == 0:
         return "No contacts."
@@ -67,7 +84,7 @@ def main():
             print(show_all(contacts))
         elif command == "help":
             print(
-                "Available commands:\nadd <name> <phone>\nchange <name> <phone>\nshow <name>\nall\nexit\n"
+                "Available commands:\nadd <name> <phone>\nchange <name> <phone>\nphone <name>\nall\nexit\n"
             )
         else:
             print("Invalid command.")
